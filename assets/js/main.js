57fc20ca195b26c3e9e46bd369df013fefb6ff53
TylerRWiddison <tyler.rwiddison@gmail.com>
2021-11-22 13:33:26 -0700

    got rid of unneccessary hamburger

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,17 +35,17 @@
 		});
 
 	// Dropdowns.
-		$('#nav > ul').dropotron({
-			alignment: 'right',
-			hideDelay: 350
-		});
+		// $('#nav > ul').dropotron({
+		// 	alignment: 'right',
+		// 	hideDelay: 350
+		// });
 
 	// Nav.
 
 		// Title Bar.
 			$(
 				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
+					// '<a href="#navPanel" class="toggle"></a>' +
 					'<span class="title">' + $('#logo').html() + '</span>' +
 				'</div>'
 			)
